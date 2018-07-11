56d41970939067f03574bc93a5e6c4f2fac507c1
haahhhaaaa <41087561+haahhhaaaa@users.noreply.github.com>
2018-07-11 08:40:35 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,31 @@
+﻿var data = [{
+	'name' : '鸿禧花园',
+	'center' : '109.428526,24.320249',
+	'type' : 1,
+	'index' :1,
+	'icon' :'img/blue.png',
+	'address': '四川成都一环路南一段1号',
+	'state': 1,
+	'cpu': '30%',
+	'subDistricts' : []
+},{
+	'name' : '金港湾',
+	'center' : '109.437902,24.305341',
+	'type' : 1,
+	'index' :2,
+	'icon' :'img/red.png',
+	'address': '四川成都一环路南一段1号',
+	'state': 2,
+	'cpu': '37%',
+	'subDistricts' : []
+},{
+	'name' : '今日田园',
+	'center' : '109.439726,24.306608',
+	'type' : 1,
+	'index' :3,
+	'icon' :'img/red.png',
+	'address': '四川成都一环路南一段1号',
+	'state': 2,
+	'cpu': '45%',
+	'subDistricts' : []
+}];
