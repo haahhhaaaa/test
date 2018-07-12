945ca6923e6301b0ccaeae2ca4324f7f9d1bb6bd
haahhhaaaa <41087561+haahhhaaaa@users.noreply.github.com>
2018-07-12 15:30:41 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,31 +1,16 @@
-﻿var data = [{
-	'name' : '客家肠粉王',
-	'center' : '109.437419,24.306808',
-	'type' : 1,
-	'index' :1,
-	'icon' :'img/blue.png',
-	'address': '柳州市鱼峰区东环大道与箭盘路东一巷交叉口南230米中国建设银行西300米',
-	'state': 1,
-	'cpu': '30%',
-	'subDistricts' : []
-},{
-	'name' : '金港湾',
-	'center' : '109.437902,24.305341',
-	'type' : 1,
-	'index' :2,
-	'icon' :'img/red.png',
-	'address': '四川成都一环路南一段1号',
-	'state': 2,
-	'cpu': '37%',
-	'subDistricts' : []
-},{
-	'name' : '今日田园',
-	'center' : '109.439726,24.306608',
-	'type' : 1,
-	'index' :3,
-	'icon' :'img/red.png',
-	'address': '四川成都一环路南一段1号',
-	'state': 2,
-	'cpu': '45%',
-	'subDistricts' : []
-}];
+var data = [
+	{'name' : '大师醉鹅','center' : '109.402808,24.344973','index' :1,'address': '营业时间：11:00—23:00','url': '<a href="https://mp.weixin.qq.com/s/AzbRb3LGAdSIjU_g3T-Cfw">详情</a>'},
+	{'name' : '肠粉王','center' : '109.437419,24.306808','index' :1,'address': '营业时间：6:30-14:00','url': '<a href="https://mp.weixin.qq.com/s/TLy8BnkbpyVeo9FmvRooCw">详情</a>'},
+	{'name' : '张飞木薯羹','center' : '109.413428,24.314718','index' :1,'address': '营业时间：10:30~24:00','url': '<a href="https://mp.weixin.qq.com/s/n6ldU3PjNo1XQKVPWLOo0w">详情</a>'},	
+	{'name' : '符老三小吃店','center' : '109.407665,24.310781','index' :1,'address': '营业时间：','url': '<a href="https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ">详情</a>'},	
+	{'name' : '黑子米粉','center' : '109.40787,24.312289','index' :1,'address': '营业时间：7月10号已经关门了','url': '<a href="https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ">详情</a>'},	
+	{'name' : '十八婶芝麻糊','center' : '109.407844,24.311405','index' :1,'address': '营业时间：','url': '<a href="https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ">详情</a>'},	
+	{'name' : '刘乐仙凉茶','center' : '109.408483,24.312327','index' :1,'address': '营业时间：','url': '<a href="https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ">详情</a>'},	
+	{'name' : '潘姐小吃','center' : '109.406379,24.312082','index' :1,'address': '','url': '<a href="https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ">详情</a>'},	
+	{'name' : '阿英米粉','center' : '109.395215,24.30267','index' :1,'address': '没有招牌的在红光小学午托中心旁','url': '<a href="https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ">详情</a>'},	
+	{'name' : '姚记冰渣豆腐花','center' : '109.410478,24.329726','index' :1,'address': '营业时间：','url': '<a href="https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ">详情</a>'},	
+	{'name' : '桂林米粉','center' : '109.377903,24.263784','index' :1,'address': '地图上的名字是旺仔桂林米粉','url': '<a href="https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ">详情</a>'},	
+	{'name' : '黃仔螺蛳粉','center' : '109.377896,24.263094','index' :1,'address': '营业时间：','url': '<a href="https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ">详情</a>'},	
+	
+
+];
