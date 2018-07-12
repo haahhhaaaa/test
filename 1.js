95ac0ec497b77a8f8d5976cae39e908babd6241c
haahhhaaaa <41087561+haahhhaaaa@users.noreply.github.com>
2018-07-12 20:17:29 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -3,7 +3,7 @@ var data = [
 	{'name' : '肠粉王','center' : '109.437419,24.306808','tel' : '' ,'index' :1,'address': '营业时间：6:30-14:00','url': 'https://mp.weixin.qq.com/s/TLy8BnkbpyVeo9FmvRooCw'},
 	{'name' : '张飞木薯羹','center' : '109.413428,24.314718','tel' : '' ,'index' :1,'address': '营业时间：10:30~24:00','url': 'https://mp.weixin.qq.com/s/n6ldU3PjNo1XQKVPWLOo0w'},	
 	{'name' : '符老三小吃店','center' : '109.407665,24.310781','tel' : '' ,'index' :1,'address': '营业时间：','url': 'https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ'},	
-	{'name' : '黑子米粉','center' : '109.408109,24.311571','tel' : '' ,'index' :1,'address': '营业时间：7月10号已经关门了','url': 'https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ'},	
+	{'name' : '黑子米粉','center' : '109.407468,24.311702','tel' : '' ,'index' :1,'address': '营业时间：7月10号已经关门了','url': 'https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ'},	
 	{'name' : '十八婶芝麻糊','center' : '109.407844,24.311405','tel' : '' ,'index' :1,'address': '营业时间：','url': 'https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ'},	
 	{'name' : '刘乐仙凉茶','center' : '109.408483,24.312327','tel' : '' ,'index' :1,'address': '营业时间：','url': 'https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ'},	
 	{'name' : '潘姐小吃','center' : '109.406379,24.312082','tel' : '' ,'index' :1,'address': '','url': 'https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ'},	
