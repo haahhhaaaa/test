4e89a0bbeb429aefb57c76342a2beb2e1c55034b
haahhhaaaa <41087561+haahhhaaaa@users.noreply.github.com>
2018-07-14 13:27:49 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -12,5 +12,22 @@ var data = [
 	{'name' : '桂林米粉','center' : '109.377903,24.263784','tel' : '' ,'index' :1,'address': '地图上的名字是旺仔桂林米粉','url': 'https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ'},	
 	{'name' : '黃仔螺蛳粉','center' : '109.377896,24.263094','tel' : '' ,'index' :1,'address': '营业时间：','url': 'https://mp.weixin.qq.com/s/aI9Xz0Z_jAMlwvWwA0YGPQ'},	
 	{'name' : '锦记石磨肠粉（古亭山店)','center' : '109.49202,24.316819','tel' : '15878277903' ,'index' :1,'address': '营业时间：6：00—18：00','url': 'https://mp.weixin.qq.com/s/x5ixeRbMFEDR9-HLn4kayw'},
-
+	{'name' : '空中花园小镇','center' : '109.411902,24.311768','tel' : '' ,'index' :1,'address': '工贸柳州商场5楼天台','url': 'https://mp.weixin.qq.com/s/5NuhTkARqVLx7z-p_FfVmA'},	
+	{'name' : '姚记豆花','center' : '109.410489,24.329609','tel' : '' ,'index' :1,'address': '营业时间：11:00~02:00','url': 'https://mp.weixin.qq.com/s/n6ldU3PjNo1XQKVPWLOo0w'},	
+	{'name' : '龚记桂林米粉(社湾店)','center' : '109.443681,24.301787','tel' : '' ,'index' :1,'address': '和杰超市对面','url': 'https://mp.weixin.qq.com/s/J_UaBjZITeQHaBmYr-kC6g'},	
+	{'name' : '张姐螺蛳粉','center' : '109.423376,24.307602','tel' : '' ,'index' :1,'address': '水南路84号 营业时间：10:00-01:00','url': 'https://mp.weixin.qq.com/s/92nJyKEpAOWp_as8HZOHuQ'},	
+	{'name' : '黄记河池煮粉','center' : '109.441339,24.32277','tel' : '' ,'index' :1,'address': '营业时间：6:00-19:00','url': 'https://mp.weixin.qq.com/s/99ycsH3l74yRoEO1SEyEWQ'},	
+	{'name' : 'NATURL多纯','center' : '109.41281,24.31321','tel' : '' ,'index' :1,'address': '营业时间：11：00~22:00','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	{'name' : '僅茶','center' : '109.41274,24.313427','tel' : '' ,'index' :1,'address': '营业时间：10:00~23:30','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	{'name' : '乌茶邦','center' : '109.412611,24.313403','tel' : '' ,'index' :1,'address': '营业时间：10:00~23:30','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	{'name' : '半兵衛','center' : '109.412928,24.31323','tel' : '13397720081' ,'index' :1,'address': '营业时间：10:00-24:00','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	{'name' : '悦茶师','center' : '109.413077,24.313245','tel' : '' ,'index' :1,'address': '营业时间：10:00~22：00','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	{'name' : '春阳茶事','center' : '109.41329,24.313083','tel' : '' ,'index' :1,'address': '营业时间：11:00~23:30','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	{'name' : '小摘堂','center' : '109.412525,24.313553','tel' : '' ,'index' :1,'address': '营业时间：10:00~23:00','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	{'name' : '一芳','center' : '109.413417,24.312694','tel' : '' ,'index' :1,'address': '一芳台湾水果茶（五星店）营业时间：10:30~23:00','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	{'name' : 'LZZY MOON夜思','center' : '109.4128,24.313286','tel' : '' ,'index' :1,'address': '饮品：11:00-24:00，宵夜：18:00-02:00','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	{'name' : 'A＋臭豆腐','center' : '109.408367,24.313485','tel' : '' ,'index' :1,'address': '营业时间：10:00-23:00','url': 'https://mp.weixin.qq.com/s/KDOnl2VDJy5euSXwuz07Mg'},	
+	{'name' : '韩国炸鸡柳','center' : '109.409127,24.313469','tel' : '' ,'index' :1,'address': '营业时间：11:00-22:30','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	{'name' : 'NATURL多纯','center' : '109.41281,24.31321','tel' : '' ,'index' :1,'address': '营业时间：11:00~02:00','url': 'https://mp.weixin.qq.com/s/7y7ORSnN275EK4KaXt5JsA'},	
+	
 ];
